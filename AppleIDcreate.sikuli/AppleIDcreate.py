7f4068222e2a3bbab57fc94c509d7fd0735a397b
Allister Banks <arubdesu@me.com>
2013-01-12 10:04:45 -0500

    path to csv changed, initial checkin

---
+++ AppleIDcreate.sikuli/AppleIDcreate.py
@@ -0,0 +1,59 @@
+import os
+import sys
+import csv
+idIndex = 1
+csvRead = []
+csvReader = csv.reader(open('/Users/Shared/ids.csv'))
+for row in csvReader:
+    csvRead.append(row)
+while idIndex < len(csvRead):
+    appleid = str(csvRead[idIndex][0])
+    password = str(csvRead[idIndex][1])
+    birthMonth = str(csvRead[idIndex][2])
+    birthDay = str(csvRead[idIndex][3])
+    birthYear = str(csvRead[idIndex][4])
+    firstName = str(csvRead[idIndex][5])
+    lastName = str(csvRead[idIndex][6])
+    streetAddress = str(csvRead[idIndex][7])
+    city = str(csvRead[idIndex][8])
+    stateCode = str(csvRead[idIndex][9])
+    zipCode = str(csvRead[idIndex][10])
+    areaCode = str(csvRead[idIndex][11]) 
+    phoneNum = str(csvRead[idIndex][12])
+    openApp("iTunes")
+    click("Screen Shot 2013-01-08 at 9.36.17 AM.png")
+    type("iBooks"+Key.ENTER)
+    # clear search field, just 'cause
+    click("1357656258285.png")
+    onAppear("iPadAppsEBoo.png", click("LiBooksBooks.png"))
+    # appleID login should appear
+    wait("SignIntodown.png")
+    click("CreateAppleI.png")
+    wait("IDclickcanon.png", 12)
+    click("IDclickcanon.png")
+    wait("innrirnnrntn.png", 12)
+    click("innrirnnrntn.png")
+    # accept terms checkbox
+    click("1357656934662.png")
+    # big details form
+    wait("EmailemaiIex.png", 12)
+    type("EmailemaiIex.png", appleid + Key.TAB + password + Key.TAB + password + 
+            Key.TAB + Key.DOWN + Key.DOWN + Key.ENTER + Key.TAB + "Newton" +
+            Key.TAB + Key.DOWN + Key.DOWN + Key.ENTER + Key.TAB + "Apple" +
+            Key.TAB + Key.DOWN + Key.DOWN + Key.DOWN + Key.ENTER + Key.TAB + "Steve" +
+            Key.TAB + Key.TAB + birthMonth + Key.ENTER + Key.TAB +
+            birthDay + Key.ENTER + Key.TAB + birthYear)
+    click("WouldNeldMn.png")
+    click("NlNi.png")
+    type(Key.PAGE_DOWN)
+    click("1357659787705.png")
+    wait("sclickhereal.png", 12)
+    click("BillingAddre.png")
+    click("Screen Shot 2013-01-08 at 12.52.37 PM.png")
+    type("BillingAddre-1.png", firstName + Key.TAB + lastName + Key.TAB + streetAddress +
+             Key.TAB + Key.TAB + city + Key.TAB + stateCode + Key.TAB + zipCode +
+             Key.TAB + areaCode + Key.TAB + phoneNum)
+    
+    click("CreateAppleI-1.png")
+    wait("1357672144168.png", 12)
+    idIndex += 1
--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 Sikuli-AutoAppleID-Creator
 ==========================
 [Sikuli](http://www.sikuli.org "Home of the Sikuli Script project") is a screen-reading automation tool which can be hooked into with Java or Python.
-This repo has a csv and sikuli script to drive iTunes and batch-create Apple IDs, inspired by http://www.enterpriseios.com/wiki/Batch_Apple_ID_Creator
+This repo has a csv and sikuli script to drive iTunes and batch-create Apple IDs, inspired by [http://www.enterpriseios.com/wiki/Batch_Apple_ID_Creator](http://www.enterpriseios.com/wiki/Batch_Apple_ID_Creator)
 Just like that predecessor, this creates IDs without a credit card, perfect for training or educational situations.
 
-NOTE: Apple may throttle the amount of IDs you can create at once. Please see the thread on enterpriseiOS for more info. Also, this has only been tested in the US version of iTunes, please submit pull requests, or contact me with screenshots of the options!
+Currently, there are limitations versus the previous, applescript-based method: SikuliScript uses java, requires focus(it assumes you have iTunes open, and are browsing the store), and I did not code in all conditionals to satisfy every permutation of the security questions Apple presents. I will discuss more of the design behind these choices on [my company's blog](http://techjournal.318.com).
+
+In the repo you'll find AppleIDcreate.sikuli and ids.csv. You must modify the ids.csv file to customize the IDs you'll be creating, and place it in /Users/Shared. Then you must have Sikuli downloaded and installed to run it.
+
+NOTE: Apple may throttle the amount of IDs you can create at once. Please see the thread on enterpriseiOS for more info. Also, this has only been tested in the US version of iTunes, please submit pull requests, or contact me with screenshots of the options for other stores around the world!
 
 Also, if you CAN have people put in a credit card to use the IDs you're tasked with creating, consider using [Fake](http://Fakeapp.com) on [Apple's site](https://appleid.apple.com/cgi-bin/WebObjects/MyAppleId.woa/wa/createAppleId?localang=en_US)
